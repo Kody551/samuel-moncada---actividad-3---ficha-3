dbe3cdf11b644a7c4c3d027c9943b9d7d981107c
Kody551 <111453400+Kody551@users.noreply.github.com>
2022-08-17 12:16:03 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,63 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                # # # # #
+                . # # . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # . . . #
+                # . . . #
+                . # # # .
+                `)
+            basic.showLeds(`
+                . # # # .
+                # # # # #
+                # # # # #
+                # # # # #
+                . # # # .
+                `)
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . . # # .
+                # # # # #
+                . . # # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+})
